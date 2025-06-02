e5963080ef0833e481a6586ce00abd0152947d30
Devin Tan <vninecoc1@gmail.com>
2025-06-02 10:52:14 +0700

    chore: merge all branch

--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -8,6 +8,7 @@ from services.summarizer import PDFSummarizer
 # Import Blueprint untuk route
 from routes.summarize import summarize_bp
 
+
 def create_app():
     app = Flask(__name__)
 
@@ -16,26 +17,29 @@ def create_app():
     print("Initializing PDFSummarizer...")
     try:
         pdf_summarizer_instance = PDFSummarizer(model_name="facebook/bart-large-cnn")
-    
-        if not hasattr(app, 'extensions'):
+
+        if not hasattr(app, "extensions"):
             app.extensions = {}
-        app.extensions['pdf_summarizer'] = pdf_summarizer_instance
-        print("Instance PDFSummarizer successfully initialized and saved in app.extensions.")
+        app.extensions["pdf_summarizer"] = pdf_summarizer_instance
+        print(
+            "Instance PDFSummarizer successfully initialized and saved in app.extensions."
+        )
     except Exception as e:
         print(f"Failed to initialize PDFSummarizer: {e}")
-        if not hasattr(app, 'extensions'):
+        if not hasattr(app, "extensions"):
             app.extensions = {}
-        app.extensions['pdf_summarizer'] = None
+        app.extensions["pdf_summarizer"] = None
 
-    @app.route('/')
+    @app.route("/")
     def index():
         return "Welcome to the PDF Summarizer API!"
 
-    app.register_blueprint(summarize_bp, url_prefix='/api')
-    
+    app.register_blueprint(summarize_bp, url_prefix="/api")
+
     return app
 
+
 # --- Jalankan Aplikasi ---
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
